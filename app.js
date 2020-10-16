e25c308d453f2aba1c971fc7fa7a6506402fdb90
Mauro Viveros <mviveros@teknos.local>
2020-10-16 01:15:58 -0300

    feat: Ruta /pruebas, para testear

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.use(bodyParser.json());
 // Configurar Cabeceras HTTP
 
 // Rutas Base
+app.get('/pruebas', (req, res)=>{
+    res.status(200).send({ message: "Hello World" });
+});
 
 module.exports = app;
